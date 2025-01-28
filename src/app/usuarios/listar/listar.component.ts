3b3ca2bcb911b5c6ca442943f59dc93d4169a7eb
jozemariajrr@gmail.com <jozemaria>
2025-01-28 17:48:05 -0300

    Atualizacao de branch develop

--- src/app/usuarios/listar/listar.component.ts
+++ src/app/usuarios/listar/listar.component.ts
@@ -1,40 +1,23 @@
-import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatModule } from 'src/app/appModules/mat.module';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { UsuariosService } from '../service/usuarios.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environment/environment';
 
 
-export interface PeriodicElement {
-  nome: string;
-  matricula: number;
-  cargo: string;
-  criacao: string;
+export interface IUsuario {
+  id: string;
+  name: string;
+  document: string;
+  graduation: string;
+  created_at: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { matricula: 165, nome: 'Hydrogen', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 223, nome: 'Helium', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 346, nome: 'Lithium', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 465, nome: 'Beryllium', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 549, nome: 'Boron', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 613, nome: 'Carbon', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 716, nome: 'Nitrogen', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 812, nome: 'Oxygen', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 912, nome: 'Fluorine', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 104, nome: 'Neon', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 115, nome: 'Sodium', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 126, nome: 'Magnesium', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 132, nome: 'Aluminum', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 143, nome: 'Silicon', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 153, nome: 'Phosphorus', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 161, nome: 'Sulfur', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 174, nome: 'Chlorine', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 187, nome: 'Argon', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 199, nome: 'Potassium', cargo: 'Soldado', criacao: '25/04/2024' },
-  { matricula: 208, nome: 'Calcium', cargo: 'Soldado', criacao: '25/04/2024' },
-];
+
 
 @Component({
   selector: 'app-listar',
@@ -43,12 +26,26 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './listar.component.html',
   styleUrl: './listar.component.scss'
 })
-export class ListarComponent implements AfterViewInit {
-  readonly router = inject(Router)
+export class ListarComponent implements OnInit, AfterViewInit {
+  ELEMENT_DATA: IUsuario[]
   displayedColumns: string[] = ['matricula', 'nome', 'cargo', 'criacao', 'actions'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  readonly router = inject(Router)
+  dataSource = new MatTableDataSource<IUsuario>;
+  lista_usuarios: IUsuario
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private usuarioService: UsuariosService,
+    private http: HttpClient) { }
+  // IMPLANTAR LOADING E SWEETALERT
+  // private router: Router,
+  // private loadingService: LoadingService,
+  // private sweetalertService: SweetalertService
+
+  ngOnInit(): void {
+    this.usuarioService.getAllUser().subscribe(res => this.dataSource = res)
+  }
+
 
   ngAfterViewInit() {
     this.paginator._intl.itemsPerPageLabel = 'Linhas por páginas'
@@ -57,6 +54,12 @@ export class ListarComponent implements AfterViewInit {
     this.paginator._intl.firstPageLabel = 'Primeira página'
     this.paginator._intl.previousPageLabel = 'Voltar página'
     this.dataSource.paginator = this.paginator;
+  }
+
+
+  onEditUser(value: number) {
+    console.log(`value`, value)
+    // this.router.navigateByUrl(`/usuario/editar/${value}`)
   }
 
   editarUsuario() {
