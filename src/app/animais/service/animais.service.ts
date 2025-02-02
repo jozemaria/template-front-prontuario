f1bf208256fa92fdf6ce00a4725e0568037b0395
jozemariajrr@gmail.com <jozemaria>
2025-02-02 02:30:25 -0300

    Inicio de integração de prontuário

--- src/app/animais/service/animais.service.ts
+++ src/app/animais/service/animais.service.ts
@@ -1,51 +1,41 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environment/environment';
 
-export interface IDadosUsuario {
-  id: number;
-  name: string;
-  email: string;
-  document: string;
-  registration: string;
-  graduation: string;
-  role: string;
-  crm: string;
-  active: boolean;
-  created_at: string;
-  updated_at: string;
-}
 @Injectable({
   providedIn: 'root'
 })
+export class AnimaisService {
+  readonly http = inject(HttpClient)
 
-export class UsuariosService {
+  private readonly urlAPI = environment.URL_BASE + 'horse_records'
 
-  private readonly urlAPI = environment.URL_BASE + 'users'
 
-  constructor(private http: HttpClient) { }
-
-  getAllUser(): Observable<any> {
+  getAllAnimal(): Observable<any> {
     return this.http.get(environment.URL_BASE + 'users', {
       headers:
         { 'Authorization': environment.TOKEN_TEST }
     })
   }
 
-  getUserById(id: number): any {
+  getAnimalById(id: number): any {
     return this.http.get(this.urlAPI + '/' + id, { headers: { 'Authorization': environment.TOKEN_TEST } })
   }
 
-  saveNewUser(user: any): any {
+  saveNewAnimal(user: any): any {
     return this.http.post(this.urlAPI, user, { headers: { 'Authorization': environment.TOKEN_TEST } })
   }
 
-  deleteUser(id: number): any {
+  deleteAnimal(id: number): any {
     return this.http.delete(this.urlAPI + '/' + id, { headers: { 'Authorization': environment.TOKEN_TEST } })
   }
 
-  updateUser(idUser: number, userUpdate: any): any {
+  updateAnimal(idUser: number, userUpdate: any): any {
     return this.http.put(this.urlAPI + '/' + idUser, userUpdate, { headers: { 'Authorization': environment.TOKEN_TEST } })
+  }
+
+  openMedicalRecord(idUser: number): any {
+    return this.http.get(this.urlAPI + '/' + idUser + '/is_active', { headers: { 'Authorization': environment.TOKEN_TEST } })
   }
 }
