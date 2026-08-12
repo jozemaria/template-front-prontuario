a96ed574385938d7c162725e55bb0e26d0f0d721
José Maria Jr <jozemariajrr@gmail.com>
2026-08-12 09:04:10 -0300

    Remoção de componentes não utilizados

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -17,40 +17,6 @@ export const Full_ROUTES: Routes = [
         path: 'forms',
         loadChildren: () => import('../../forms/forms.module').then(m => m.FormsModule)
     },
-
-    {
-        path: 'tables',
-        loadChildren: () => import('../../tables/tables.module').then(m => m.TablesModule)
-    },
-
-    {
-        path: 'widgets',
-        loadChildren: () => import('../../widgets/widgets.module').then(m => m.WidgetsModule)
-    },
-
-    {
-        path: 'charts',
-        loadChildren: () => import('../../charts/charts.module').then(m => m.ChartsModule)
-    },
-
-    {
-        path: 'faq',
-        loadChildren: () => import('../../faq/faq.module').then(m => m.FaqModule)
-    },
-    {
-        path: 'profile',
-        loadChildren: () => import('../../profile/profile.module').then(m => m.ProfileModule)
-    },
-    {
-        path: 'pricing',
-        loadChildren: () => import('../../pricing/pricing.module').then(m => m.PricingModule)
-    },
-
-    {
-        path: 'downloads',
-        loadChildren: () => import('../../downloads/downloads.module').then(m => m.DownloadsModule)
-    },
-
     {
         path: 'usuarios',
         loadChildren: () => import('../../usuarios/usuarios.module').then(m => m.UsuariosModule)
